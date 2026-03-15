05e3b8d02a4827223bb375c0a031729b8cce45a1
Your Name <you@example.com>
2026-03-15 18:52:12 +0800

    failture

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
+#!/bin/bash
 java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.3.0/unix_args.txt "$@"
